b1c0a71044893a6b9a121c5021933ffb39b45e8b
memoblancanieves <ing.or.alvarez.vel@gmail.com>
2019-04-22 16:12:57 -0500

    resubido

---
+++ resources/views/prueba/prueba.blade.php
@@ -0,0 +1,27 @@
+ 
+ @extends('layout.content')
+
+@section('content')
+            <div class="page-content-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div style="min-height: 300px;">
+                                        <table class="table table-bordered">
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection

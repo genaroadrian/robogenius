ec1db9863ca35c19cef346214da86ae7e0e7e2c2
genaroadrian <al221711695@gmail.com>
2019-04-23 12:50:00 -0500

    personalcrud

---
+++ app/Personal.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace slidecom_robogenius;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Personal extends Model
+{
+    public function getKeyName(){
+        return "idper";
+    }
+
+    public $timestamps = false;
+    protected $table = "personal";
+    protected $fillable = [ 
+    	'idper', 
+    	'nombre',
+        'apellidos',
+        'usuario',
+        'contra',
+        'fechanac',
+        'sexo',
+        'curp',
+        'estadocivil',
+        'domicilio',
+        'fechaingreso',
+        'horasalida',
+        'horaentrada',
+        'perfilprofesional',
+        'especialidad',
+        'salariomensual',
+        'tareasasignadas',
+        'activo'
+    ];
+}

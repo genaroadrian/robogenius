a7d9cf4aa01e9d54beb7508acfc8e0243b862d57
Erick-Robogenius <al221710827@gmail.com>
2019-04-24 15:56:58 -0500

    /home

---
+++ angular/src/app/home-personal/home-personal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-personal',
+  templateUrl: './home-personal.component.html',
+  styleUrls: ['./home-personal.component.css']
+})
+export class HomePersonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

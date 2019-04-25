e287514b4d7ee6e6b4968837abc032a3960c78b3
genaroadrian <al221711695@gmail.com>
2019-04-25 15:31:08 -0500

    angularcorrectin

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,12 +7,8 @@ import {HttpClientModule} from '@angular/common/http';
 import { HomePersonalComponent } from './home-personal/home-personal.component';
 import { FormPersonalComponent } from './form-personal/form-personal.component';
 import {Route,RouterModule} from '@angular/router';
-<<<<<<< HEAD
-import {HttpClientModule} from '@angular/common/http';
-=======
 import { HomeTipopersonalComponent } from './home-tipopersonal/home-tipopersonal.component';
 import { FormTipopersonalComponent } from './form-tipopersonal/form-tipopersonal.component';
->>>>>>> 3824d440820129b0f3173dfde9696c75e7cbadfc
 const routes: Route[] = [
 {path:'personal',component:HomePersonalComponent},
 {path:'personalform',component:FormPersonalComponent},
@@ -25,19 +21,13 @@ const routes: Route[] = [
     HomePersonalComponent,
     FormPersonalComponent,
     HomeTipopersonalComponent,
-    FormTipopersonalComponent,
-    
+    FormTipopersonalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-	RouterModule.forRoot(routes),
-	HttpClientModule
-=======
     RouterModule.forRoot(routes),
     HttpClientModule
->>>>>>> 3824d440820129b0f3173dfde9696c75e7cbadfc
   ],
   providers: [],
   bootstrap: [AppComponent]

dab4ee807857e84fdb9815ba54e26703db302361
genaroadrian <al221711695@gmail.com>
2019-04-25 14:49:54 -0500

    personalinterface

--- angular/src/app/form-personal/form-personal.component.ts
+++ angular/src/app/form-personal/form-personal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TipopersonalService } from '../services/tipopersonal.service';
+import { HttpClient } from '@angular/common/http';
+import {Tipopersonal} from '../interfaces/tipopersonal';
 
 @Component({
   selector: 'app-form-personal',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-personal.component.css']
 })
 export class FormPersonalComponent implements OnInit {
-
-  constructor() { }
+  tipopersonal: Tipopersonal[];
+  constructor(private tipopersonalService: TipopersonalService, private httpClient: HttpClient) {
+    this.tipopersonalService.get().subscribe((data: Tipopersonal[])=>{
+      this.tipopersonal = data;
+    },(error)=>{
+      console.log(error);
+      alert('Ocurrio un error');
+    });
+   }
 
   ngOnInit() {
   }

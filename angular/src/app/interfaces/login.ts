a5dbd79fd598839dad6555a45bab415b83e6e7cc
Erick-Robogenius <al221710827@gmail.com>
2019-06-24 14:18:18 -0500

    Login

---
+++ angular/src/app/interfaces/login.ts
@@ -0,0 +1,11 @@
+export interface Login {
+    id?: number;
+    subname: string;
+    email: string;
+    password: string;
+    nombre: string;
+    apellidos: string;
+    telefono: number;
+    avatar: string;
+    activo: number;
+}

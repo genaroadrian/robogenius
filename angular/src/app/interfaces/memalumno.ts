f3d9d3c02f204ecc132f887fa07d90aed9c35151
Erick-Robogenius <al221710827@gmail.com>
2019-07-02 12:10:59 -0500

    Rutas

--- angular/src/app/interfaces/memalumno.ts
+++ angular/src/app/interfaces/memalumno.ts
@@ -6,19 +6,18 @@ export interface Memalumnos {
     adelanto: number,
     restante: number,
     total: number
-// =======
-// export interface Memalumno {
-//     Id?: number;
-//     Membrecia: string;
-//     Alumno: string;
-//     Pago: number;
-//     Adelanto: number;
-//     Restante: number;
-//     Total: number;
-//     InicioMembrecia: string;
-//     idalu: number;
-//     idtipopago: number;
-//     idtmem: number;
-//     activo: number;
-// >>>>>>> 9f61d079dd688f3e5027cd6837db45a7fecc81e4
+}
+export interface Memalumno {
+    Id?: number;
+    Membrecia: string;
+    Alumno: string;
+    Pago: number;
+    Adelanto: number;
+    Restante: number;
+    Total: number;
+    InicioMembrecia: string;
+    idalu: number;
+    idtipopago: number;
+    idtmem: number;
+    activo: number;
 }

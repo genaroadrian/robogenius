104b2ed80eb8f3e958315ea2b608c03ac00bee24
Erick-Robogenius <al221710827@gmail.com>
2019-09-02 09:42:54 -0500

    asdfghjkl

---
+++ app/Http/Controllers/fotoAlumnosController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace slidecom_robogenius\Http\Controllers;
+
+use Illuminate\Http\Request;
+use slidecom_robogenius\Http\Controllers\Controller;
+use slidecom_robogenius\Alumno;
+use Illuminate\Support\Facades\DB;
+
+class fotoAlumnosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $alumno = Alumno::find($id);
+        $alumno->perfilalu = $request->perfilalu;
+        $alumno->save();
+        echo json_encode($alumno);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

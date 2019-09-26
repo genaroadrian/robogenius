b9933e48b3ca0365b38fda679aac3501974258fb
Erick-Robogenius <al221710827@gmail.com>
2019-09-26 11:08:12 -0500

    jajajajejej

---
+++ angular/src/app/login/alumnoslogin/alumnoslogin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnoslogin',
+  templateUrl: './alumnoslogin.component.html',
+  styleUrls: ['./alumnoslogin.component.css']
+})
+export class AlumnosloginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

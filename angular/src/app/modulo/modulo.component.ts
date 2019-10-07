216cd3652ffa9a4eef79ac94aca7f010721455f8
genaroadrian <al221711695@gmail.com>
2019-10-07 13:32:03 -0500

    Modulo de clases 1.0.1

--- angular/src/app/modulo/modulo.component.ts
+++ angular/src/app/modulo/modulo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
-import { FormControl, Validators,  FormBuilder } from '@angular/forms';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { NivelService } from '../services/nivel.service';
 import { ToastrManager } from 'ng6-toastr-notifications';
@@ -20,6 +20,7 @@ import { AreaaddComponent } from '../areadelconocimiento/areaadd/areaadd.compone
 import { tema } from '../interfaces/tema';
 import { TemaaddComponent } from '../tema/temaadd/temaadd.component';
 import { TemaService } from '../services/tema.service';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -28,12 +29,12 @@ import { TemaService } from '../services/tema.service';
   styleUrls: ['./modulo.component.css'],
   providers: [{
     provide: STEPPER_GLOBAL_OPTIONS, useValue: { showError: true }
-  }]
-  
+  }, DatePipe]
+
 })
 export class ModuloComponent implements OnInit {
 
-  
+
 
 
 
@@ -48,85 +49,93 @@ export class ModuloComponent implements OnInit {
   subtemas: Object;
   datos: any;
   areadelconocimiento: any;
+  herramientas: any
+  herramienta: any //Valor del select de herramientas
+  subareas: any
+  subarea: any //Valor del select de subarea del conocimiento
   tema: any;
   subtema: any;
+  today:any
 
 
 
 
 
-  constructor( public dialog: MatDialog, private nivelService : NivelService, public toastr: ToastrManager, public notifications: NotificationsService,
-    private _formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router, private gradoService : GradoService, private areaService : AreadelconocimientoService,
-    private moduloService : ModuloService, private temaService : TemaService ) { }
+  constructor(public dialog: MatDialog, private nivelService: NivelService, public toastr: ToastrManager, public notifications: NotificationsService,
+    private _formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router, private gradoService: GradoService, private areaService: AreadelconocimientoService,
+    private moduloService: ModuloService, private temaService: TemaService,private datePipe: DatePipe) { }
 
   ngOnInit() {
+    this.fechaHoy()
+    this.getHerramientas()
     this.get()
     //Obtener datos de modal nivel
-    this.nivelService.get().subscribe((data)=>{
-      console.log(data)
+    this.nivelService.get().subscribe((data) => {
+      // console.log(data)
       this.persona = data
-    },(error)=>{
+    }, (error) => {
     })
     //Obtener datos de modal grados
-    this.gradoService.get().subscribe((data)=>{
-      console.log(data)
+    this.gradoService.get().subscribe((data) => {
+      // console.log(data)
       this.grado = data
-    },(error)=>{
+    }, (error) => {
     })
-    this.moduloService.geta().subscribe((data)=>{
-      console.log(data)
+    this.moduloService.geta().subscribe((data) => {
+      // console.log(data)
       this.areas = data
-    },(error)=>{
+    }, (error) => {
     })
-    this.moduloService.gett().subscribe((data)=>{
-      console.log(data)
+    this.moduloService.gett().subscribe((data) => {
+      // console.log(data)
       this.temas = data
-    },(error)=>{
+    }, (error) => {
     })
-    this.moduloService.gets().subscribe((data)=>{
-      console.log(data)
+    this.moduloService.gets().subscribe((data) => {
+      // console.log(data)
       this.subtemas = data
-    },(error)=>{
+    }, (error) => {
     })
-   
-   
+
+    this.getSubAC()
+
+
 
     //
- 
-   
+
+
   }
 
- /* onChangeArea(idac: number) {
-    if (idac) {
-      this.moduloService.gett().subscribe(
-        data => {
-          this.temass = data;
-          this.subtemass = null;
-        }
-      );
-    } else {
-      this.temass = null;
-      this.subtemass = null;
-    }
-  }*/
+  /* onChangeArea(idac: number) {
+     if (idac) {
+       this.moduloService.gett().subscribe(
+         data => {
+           this.temass = data;
+           this.subtemass = null;
+         }
+       );
+     } else {
+       this.temass = null;
+       this.subtemass = null;
+     }
+   }*/
 
 
-  
-   // Notificación de success al eliminar
-   showSuccesSave() {
-    this.toastr.successToastr('Registro guardado','Exito!');
+
+  // Notificación de success al eliminar
+  showSuccesSave() {
+    this.toastr.successToastr('Registro guardado', 'Exito!');
   }
-      /* Mostrar la barra de carga */
-      showBarra() {
-        this.barra = ""
-      }
-      hideBarra() {
-        this.barra = "none"
-      }
-   
+  /* Mostrar la barra de carga */
+  showBarra() {
+    this.barra = ""
+  }
+  hideBarra() {
+    this.barra = "none"
+  }
 
-  nuev(nivelest: Niveles)
-  {
+
+  nuev(nivelest: Niveles) {
     /* abrir un pequeño modal para agregar otro tipo de personal */
     const dialogRef = this.dialog.open(AddnivelComponent, {
       data: { nivelest: nivelest }
@@ -135,7 +144,7 @@ export class ModuloComponent implements OnInit {
       if (result == 1) {
         this.showBarra()
         this.nivelService.add(this.nivelService.getDialogData()).subscribe((data) => {
-         this.persona.push(data)  
+          this.persona.push(data)
           this.notifications.showSuccessAdd();
           this.hideBarra();
         }, (error) => {
@@ -148,8 +157,7 @@ export class ModuloComponent implements OnInit {
       }
     });
   }
-  gradomodal(grado: Grados)
-  {
+  gradomodal(grado: Grados) {
     /* abrir un pequeño modal para agregar otro tipo de personal */
     const dialogRef = this.dialog.open(AddgradosComponent, {
       data: { grado: grado }
@@ -158,7 +166,7 @@ export class ModuloComponent implements OnInit {
       if (result == 1) {
         this.showBarra()
         this.gradoService.addd(this.gradoService.getDialogData()).subscribe((data) => {
-         this.grado.push(data)  
+          this.grado.push(data)
           this.notifications.showSuccessAdd();
           this.hideBarra();
         }, (error) => {
@@ -172,8 +180,7 @@ export class ModuloComponent implements OnInit {
     });
   }
 
-  areamodal(area: areadelconocimiento)
-  {
+  areamodal(area: areadelconocimiento) {
     /* abrir un pequeño modal para agregar otro tipo de personal */
     const dialogRef = this.dialog.open(AreaaddComponent, {
       data: { area: area }
@@ -182,7 +189,7 @@ export class ModuloComponent implements OnInit {
       if (result == 1) {
         this.showBarra()
         this.areaService.addd(this.areaService.getDialogData()).subscribe((data) => {
-         this.areas.push(data)  
+          this.areas.push(data)
           this.notifications.showSuccessAdd();
           this.hideBarra();
         }, (error) => {
@@ -196,9 +203,8 @@ export class ModuloComponent implements OnInit {
     });
   }
 
-  
-  temamodal(tema: tema)
-  {
+
+  temamodal(tema: tema) {
     /* abrir un pequeño modal para agregar otro tipo de personal */
     const dialogRef = this.dialog.open(TemaaddComponent, {
       data: { tema: tema }
@@ -207,7 +213,7 @@ export class ModuloComponent implements OnInit {
       if (result == 1) {
         this.showBarra()
         this.temaService.addd(this.areaService.getDialogData()).subscribe((data) => {
-         this.temas.push(data)  
+          this.temas.push(data)
           this.notifications.showSuccessAdd();
           this.hideBarra();
         }, (error) => {
@@ -220,37 +226,62 @@ export class ModuloComponent implements OnInit {
       }
     });
   }
-  get(){
-    this.areaService.getall().subscribe((data)=>{
-      this.datos=data;
+  get() {
+    this.areaService.getall().subscribe((data) => {
+      this.datos = data;
       var hash = {};
-      this.areadelconocimiento = this.datos.filter(function(area) {
+      this.areadelconocimiento = this.datos.filter(function (area) {
         var exists = !hash[area.nomarea] || false;
         hash[area.nomarea] = true;
         return exists;
       });
-    }) 
+    })
 
-    
+
   }
-  gettema(AREA){
-    this.tema=this.areadelconocimiento.filter(tem=> tem.idac==AREA)
-    console.log(AREA);
-    console.log(this.tema);
-      var hash = {};
-      this.tema = this.tema.filter(function(tem) {
-        var exists = !hash[tem.nomtema] || false;
-        hash[tem.nomtema] = true;
-        return exists;
-        
-    }) 
-      
+  gettema(AREA) {
+    this.tema = this.areadelconocimiento.filter(tem => tem.idac == AREA)
+    // console.log(AREA);
+    // console.log(this.tema);
+    var hash = {};
+    this.tema = this.tema.filter(function (tem) {
+      var exists = !hash[tem.nomtema] || false;
+      hash[tem.nomtema] = true;
+      return exists;
+
+    })
+
   }
-  getsubtema(TEMA){
-   console.log(this.tema)
-    this.subtema=this.datos.filter(sub=> sub.idt==TEMA)
-    console.log(TEMA);
-    console.log(this.subtema);
+
+  getSubAC() {
+    this.moduloService.getSAC().subscribe((data) => {
+      console.log(data)
+      this.subareas = data
+    }, (error) => {
+
+    })
+  }
+
+  getsubtema(TEMA) {
+    // console.log(this.tema)
+    this.subtema = this.datos.filter(sub => sub.idt == TEMA)
+    // console.log(TEMA);
+    // console.log(this.subtema);
+  }
+
+  getHerramientas() {
+    this.moduloService.getHerra().subscribe((data) => {
+      console.log(data)
+      this.herramientas = data
+    }, (error) => {
+
+    })
+  }
+
+  fechaHoy()
+  {
+    this.today = new Date()
+    this.today = this.datePipe.transform(this.today, 'yyyy-MM-dd')
   }
 
 
--- angular/src/app/services/modulo.service.ts
+++ angular/src/app/services/modulo.service.ts
@@ -10,17 +10,28 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 })
 export class ModuloService {
   API_ENDPOINT = 'http://localhost:8000/api';
- 
+
   constructor(private httpClient: HttpClient) { }
 
   geta() {
     return this.httpClient.get(this.API_ENDPOINT + '/areadelconocimiento');
- }
- gett() {
-  return this.httpClient.get(this.API_ENDPOINT + '/tema');
-}
-gets() {
-  return this.httpClient.get(this.API_ENDPOINT + '/subtema');
-}
-  
+  }
+  gett() {
+    return this.httpClient.get(this.API_ENDPOINT + '/tema');
+  }
+  gets() {
+    return this.httpClient.get(this.API_ENDPOINT + '/subtema');
+  }
+
+  getSAC()
+  {
+    return this.httpClient.get(this.API_ENDPOINT+'/subareac')
+  }
+
+  getHerra()
+  {
+    return this.httpClient.get(this.API_ENDPOINT+'/herramientas')
+    return this.httpClient.get(this.API_ENDPOINT+'/herramientas')
+  }
+
 }

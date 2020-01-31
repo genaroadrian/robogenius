4829cdd10a4573ae406559d1e266aad0d75a1737
Genaro Adrian <genaroadrian98@gmail.com>
2020-01-31 13:34:06 -0600

    cambios finales

--- angular/src/app/alumnos/inactivealu/inactivealu.component.ts
+++ angular/src/app/alumnos/inactivealu/inactivealu.component.ts
@@ -12,78 +12,13 @@ import { startWith, switchMap, map, catchError, observeOn } from 'rxjs/operators
 })
 export class InactivealuComponent implements AfterViewInit {
 
-  displayedColumns: string[] = [
-    'name',
-    'age',
-    'sex',
-    'icons'
-  ];
-
-  exampleDatabase: ExampleHttpDatabase | null
-  data: InactiveStudents[] = []
-  
-  
-  resultLength = 0
-  isLoadingResults = true
-  isRateLimitReached = false
-
-  dataSource: MatTableDataSource<InactiveStudents>
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-
+ 
   constructor(public alumnosService: AlumnosService) {
   }
 
   ngAfterViewInit() {
-    this.exampleDatabase = new ExampleHttpDatabase(this.alumnosService)
-
-    this.sort.sortChange.subscribe(()=> this.paginator.pageIndex = 0)
-
-    merge(this.sort.sortChange, this.paginator.page)
-    .pipe(
-      startWith({}),
-      switchMap(()=>{
-        this.isLoadingResults = true
-        return this.exampleDatabase!.getAlumnos(
-          this.sort.active, this.sort.direction, this.paginator.pageIndex)
-      }), map(data =>{
-        console.log('la data es')
-        console.log(data)
-        // let d: any = data
-        this.isLoadingResults = false
-        this.isRateLimitReached = false
-        this.resultLength = data.total_count
-
-        return data.items
-      }),
-      catchError(()=>{
-        this.isLoadingResults = false
-        this.isRateLimitReached = true
-        return observableOf([])
-      })
-    ).subscribe(data => this.data = data)
+    
   }
 
-
-}
-
-export interface Api
-{
-  items: InactiveStudents[],
-  total_count: number
-}
-
-
-export class ExampleHttpDatabase
-{
-  constructor(public alumnosService: AlumnosService)
-  {
-
-  }
-
-  async getAlumnos(sort: string, order: string, page: number)
-  {
-    return await this.alumnosService.fetchInactive().toPromise()
-  }
 
 }
